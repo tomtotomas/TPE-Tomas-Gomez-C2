a3dea3d529aabd1c22d7c5a74ac4c3cebeffd033
Tomas Fucking Gomez <tomas.a.gomez.zb@gmail.com>
2025-09-23 19:50:48 -0300

    Actualice algunas cosas en la db y agregue datos como ejemplo

--- db_music.sql
+++ db_music.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2025 a las 23:13:09
+-- Tiempo de generación: 24-09-2025 a las 00:43:52
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -35,6 +35,13 @@ CREATE TABLE `albums` (
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `albums`
+--
+
+INSERT INTO `albums` (`album_id`, `album_name`, `album_image`, `artist_id`, `date`) VALUES
+(1, 'Fiesta para Hamburguesas', 'https://imgur.com/a/qIjjmTz', 1, '1998-09-15');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +53,13 @@ CREATE TABLE `artist` (
   `artist_name` varchar(150) NOT NULL,
   `artist_bio` varchar(1082) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `artist`
+--
+
+INSERT INTO `artist` (`artist_id`, `artist_name`, `artist_bio`) VALUES
+(1, 'LA CUMBIA DEL LEMON', 'LA CUMBIA DEL LEMON es una banda que surge durante los turbulantes años de los 90s. Conformada por Ignacio Bianchi como vocalista y guitarrista. Felipe Fermin Fernandez como teclasita. Y Jimena Gilbert en la bateria.');
 
 -- --------------------------------------------------------
 
@@ -62,6 +76,13 @@ CREATE TABLE `reviews` (
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `reviews`
+--
+
+INSERT INTO `reviews` (`review_id`, `user_id`, `album_id`, `review`, `rating`, `date`) VALUES
+(1, 1, 1, 'Lo dificil de analizar un album viejo es que uno no puede ser critico sin ser juzgado por quizas pecar de distinto, pero \"Fiesta para Hamburguesas\" es honestamente un mal album, o eso podria pensar, pero uno llega a \"Hamburguesa despues de la hamburguesa\" y se da cuenta que este album es un viaje en el que todo termina de conectar ahi. Una expericiena de otro mundo.', 4, '2025-09-10');
+
 -- --------------------------------------------------------
 
 --
@@ -71,9 +92,23 @@ CREATE TABLE `reviews` (
 CREATE TABLE `songs` (
   `song_id` int(11) NOT NULL,
   `album_id` int(11) NOT NULL,
+  `song_number` int(11) NOT NULL,
   `song_name` varchar(150) NOT NULL,
   `duration` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `songs`
+--
+
+INSERT INTO `songs` (`song_id`, `album_id`, `song_number`, `song_name`, `duration`) VALUES
+(1, 1, 1, 'Pan Lactal para la hamburguesa', '03:20'),
+(2, 1, 2, '¿Dulce de leche en el paty?', '04:20'),
+(3, 1, 3, 'Sin lechuga, por favor', '05:20'),
+(4, 1, 4, 'El payaso Ronald Mcdonalds', '01:25'),
+(5, 1, 5, 'Mejor dos hamburguesas que una', '02:50'),
+(6, 1, 6, 'Hamburguesa despues de la hamburguesa', '07:28'),
+(7, 1, 7, 'Ya no mas hamburguesas', '02:20');
 
 -- --------------------------------------------------------
 
@@ -87,6 +122,13 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
+(1, 'gordomusical199', 'megustalamusica1997@hotmail.com', 'quecaposoy123');
 
 --
 -- Índices para tablas volcadas
@@ -136,31 +178,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `albums`
 --
 ALTER TABLE `albums`
-  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `artist`
 --
 ALTER TABLE `artist`
-  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
